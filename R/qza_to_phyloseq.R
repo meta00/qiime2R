5889c31250da6923d61f080db9c5d1920aecd31e
meta00 <hobichhai@gmail.com>
2019-08-13 14:21:46 +0700

    fix space

--- R/qza_to_phyloseq.R
+++ R/qza_to_phyloseq.R
@@ -9,63 +9,59 @@
 #' @param tmp a temporary directory that the object will be decompressed to (default="/tmp")
 #' @return a phyloseq object
 #'
-#' @examples \dontrun{physeq<-qza_to_phyloseq(features="data/table.qza", tree="data/rooted-tree.qza", taxonomy="data/taxonomy.qza", metdata="data/sample-metadata.qza")}
+#' @examples \dontrun{physeq <- qza_to_phyloseq(features="data/table.qza", tree="data/rooted-tree.qza", taxonomy="data/taxonomy.qza", metdata="data/sample-metadata.qza")}
 #' @export
 #'
 #'
 #'
 
-qza_to_phyloseq<-function(features,tree,taxonomy,metadata, tmp){
+qza_to_phyloseq <- function(features,tree,taxonomy,metadata, tmp){
 
    if(missing(features) & missing(tree) & missing(taxonomy) & missing(metadata)){
     stop("At least one required artifact is needed (features/tree/taxonomy/) or the metadata.")
    }
-  
-if(missing(tmp)){tmp <- tempdir()}
 
-  
-
-
-  argstring<-""
+if(missing(tmp)){tmp  <-  tempdir()}
+  argstring <- ""
 
   if(!missing(features)){
-    features<-read_qza(features, tmp=tmp)$data
-    argstring<-paste(argstring, "otu_table(features, taxa_are_rows=T),")
+    features <- read_qza(features, tmp=tmp)$data
+    argstring <- paste(argstring, "otu_table(features, taxa_are_rows=T),")
   }
 
   if(!missing(taxonomy)){
-    taxonomy<-read_qza(taxonomy, tmp=tmp)$data
-    taxt<-strsplit(as.character(taxonomy$Taxon),"\\; ")
-    taxt<-lapply(taxt, function(x){length(x)=7;return(x)})
-    taxt<-do.call(rbind, taxt)
-    taxt<-apply(taxt,2, function(x) replace(x, grepl("^[kpcofgs]__$", x), NA))
-    rownames(taxt)<-taxonomy$Feature.ID
-    colnames(taxt)<-c("Kingdom","Phylum","Class","Order","Family","Genus","Species")
-    argstring<-paste(argstring, "tax_table(taxt),")
+    taxonomy <- read_qza(taxonomy, tmp=tmp)$data
+    taxt <- strsplit(as.character(taxonomy$Taxon),"\\; ")
+    taxt <- lapply(taxt, function(x){length(x)=7;return(x)})
+    taxt <- do.call(rbind, taxt)
+    taxt <- apply(taxt,2, function(x) replace(x, grepl("^[kpcofgs]__$", x), NA))
+    rownames(taxt) <- taxonomy$Feature.ID
+    colnames(taxt) <- c("Kingdom","Phylum","Class","Order","Family","Genus","Species")
+    argstring <- paste(argstring, "tax_table(taxt),")
   }
 
   if(!missing(tree)){
-    tree<-read_qza(tree, tmp=tmp)$data
-    argstring<-paste(argstring, "phy_tree(tree),")
+    tree <- read_qza(tree, tmp=tmp)$data
+    argstring <- paste(argstring, "phy_tree(tree),")
   }
 
   if(!missing(metadata)){
-    
-    defline<-suppressWarnings(readLines(metadata)[2])
+
+    defline <- suppressWarnings(readLines(metadata)[2])
     if(grepl("^#q2:types", defline)){
-      metadata<-read_q2metadata(metadata)
-      rownames(metadata)<-metadata$SampleID
-      metadata$SampleID<-NULL
+      metadata <- read_q2metadata(metadata)
+      rownames(metadata) <- metadata$SampleID
+      metadata$SampleID <- NULL
     } else{
-      metadata<-read.table(metadata, row.names=1, sep='\t', quote="", header=TRUE)
+      metadata <- read.table(metadata, row.names=1, sep='\t', quote="", header=TRUE)
     }
-    argstring<-paste(argstring, "sample_data(metadata),")
+    argstring <- paste(argstring, "sample_data(metadata),")
     sample_data(metadata)
   }
 
-  argstring<-gsub(",$","", argstring) #remove trailing ","
+  argstring <- gsub(",$","", argstring) #remove trailing ","
 
-  physeq<-eval(parse(text=paste0("phyloseq(",argstring,")")))
+  physeq <- eval(parse(text=paste0("phyloseq(",argstring,")")))
 
   return(physeq)
 }
